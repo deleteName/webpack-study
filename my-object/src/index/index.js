f0e6256e9618725c9683c9e15eb66c221b6dcb4d
churui_vip@163.com <www574442497==>
2021-03-15 22:59:17 +0800

    同步代码

---
+++ my-object/src/index/index.js
@@ -0,0 +1,3 @@
+import { helloworld } from './hello'
+
+document.body.innerText = helloworld()
---
+++ my-object/src/search/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './search.less'
+import logo from '../assets/aowo.png'
+
+class Search extends React.Component {
+
+    render () {
+        return <div className="search-text">
+            <span>搜索文字Watch搜索</span>
+            <img src={logo} />
+        </div>
+    }
+}
+
+ReactDOM.render(
+    <Search></Search>,
+    document.querySelector('#root')
+)
